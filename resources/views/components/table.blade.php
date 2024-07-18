9514d42d897bde54826663ec5fcb4c14c49b6bf0
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2024-07-18 10:13:50 -0400

    fix view: fix del componente table del padding

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col">
     <div class="-m-1.5 overflow-x-auto">
-        <div class="p-1.5 min-w-full inline-block align-middle">
+        <div class="p-3 min-w-full inline-block align-middle py-4">
             <div class="border rounded-lg shadow overflow-hidden">
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
