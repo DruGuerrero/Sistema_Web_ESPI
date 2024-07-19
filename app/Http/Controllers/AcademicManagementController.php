89492a8cd5eac95d8c1aaaab8c9a6ec863114855
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2024-07-18 21:17:09 -0400

    Feature: Implementado el controlador que obtiene datos de las carreras desde moodle a traves de solicitudes https

--- app/Http/Controllers/AcademicManagementController.php
+++ app/Http/Controllers/AcademicManagementController.php
@@ -2,25 +2,109 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Cache;
 
 class AcademicManagementController extends Controller
 {
     public function index()
     {
-        // Obtener las carreras de la base de datos, por ahora usamos datos ficticios.
-        $careers = [
-            ['name' => 'Enfermería', 'students' => 999, 'description' => 'Esta es la descripción de la carrera o curso. Descripción. Descripción.'],
-            ['name' => 'Enfermería', 'students' => 999, 'description' => 'Esta es la descripción de la carrera o curso. Descripción. Descripción.'],
-            ['name' => 'Enfermería', 'students' => 999, 'description' => 'Esta es la descripción de la carrera o curso. Descripción. Descripción.'],
-            ['name' => 'Enfermería', 'students' => 999, 'description' => 'Esta es la descripción de la carrera o curso. Descripción. Descripción.'],
-            ['name' => 'Enfermería', 'students' => 999, 'description' => 'Esta es la descripción de la carrera o curso. Descripción. Descripción.'],
-            // Agregar más carreras según sea necesario.
-        ];
+        $apikey = Config::get('app.moodle_api_key');
+
+        // Intentar obtener de la caché primero
+        $careers = Cache::remember('moodle_careers', 60, function () use ($apikey) {
+            $careers = [];
+
+            // Obtener categorías padre
+            $categoriesResponse = Http::post('https://campusespi.gcproject.net/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_course_get_categories'
+                . '&wstoken=' . urldecode($apikey)
+                . '&criteria[0][key]=parent'
+                . '&criteria[0][value]=0'
+            );
+
+            if ($categoriesResponse->failed()) {
+                Log::error('Error obteniendo categorías de Moodle: ' . $categoriesResponse->body());
+                return []; // Retornar un array vacío en caso de error
+            }
+
+            $categories = $categoriesResponse->json();
+
+            foreach ($categories as $category) {
+                if ($category['parent'] != 0) {
+                    continue; // Asegurarse de que solo se procesan las categorías padre
+                }
+                
+                $categoryId = $category['id'];
+                $categoryName = $category['name'];
+                $categoryDescription = strip_tags($category['description']); // Eliminar etiquetas HTML
+
+                // Inicializar el total de estudiantes
+                $totalStudents = 0;
+
+                // Obtener categorías hijo
+                $subCategoriesResponse = Http::post('https://campusespi.gcproject.net/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_course_get_categories'
+                    . '&wstoken=' . urldecode($apikey)
+                    . '&criteria[0][key]=parent'
+                    . '&criteria[0][value]=' . $categoryId
+                );
+
+                if ($subCategoriesResponse->failed()) {
+                    Log::error('Error obteniendo subcategorías de Moodle: ' . $subCategoriesResponse->body());
+                    continue; // Saltar esta categoría padre en caso de error
+                }
+
+                $subCategories = $subCategoriesResponse->json();
+
+                // Procesar cada subcategoría para contar los estudiantes
+                foreach ($subCategories as $subCategory) {
+                    // Obtener cursos por subcategoría
+                    $coursesResponse = Http::post('https://campusespi.gcproject.net/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_course_get_courses_by_field'
+                        . '&wstoken=' . urldecode($apikey)
+                        . '&field=category'
+                        . '&value=' . $subCategory['id']
+                    );
+
+                    if ($coursesResponse->failed()) {
+                        Log::error('Error obteniendo cursos de Moodle: ' . $coursesResponse->body());
+                        continue; // Saltar esta subcategoría en caso de error
+                    }
+
+                    $courses = $coursesResponse->json()['courses'];
+
+                    // Contar estudiantes matriculados en cada curso
+                    foreach ($courses as $course) {
+                        $courseId = $course['id'];
+                        $enrolledUsersResponse = Http::post('https://campusespi.gcproject.net/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=core_enrol_get_enrolled_users'
+                            . '&wstoken=' . urldecode($apikey)
+                            . '&courseid=' . $courseId
+                        );
+
+                        if ($enrolledUsersResponse->failed()) {
+                            Log::error('Error obteniendo estudiantes matriculados en Moodle: ' . $enrolledUsersResponse->body());
+                            continue; // Saltar este curso en caso de error
+                        }
+
+                        $enrolledUsers = $enrolledUsersResponse->json();
+                        $totalStudents += count($enrolledUsers);
+                    }
+                }
+
+                // Solo agregar la categoría padre al array de carreras
+                $careers[] = [
+                    'name' => $categoryName,
+                    'students' => $totalStudents,
+                    'description' => $categoryDescription,
+                ];
+
+                Log::info('Parent category added: ' . $categoryName);
+            }
+
+            return $careers;
+        });
 
         return view('web.admin.academic.index', compact('careers'));
     }
-
-    // Aquí puedes agregar más métodos como create, store, edit, update, destroy
 }
--- resources/views/web/admin/academic/index.blade.php
+++ resources/views/web/admin/academic/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Gestión Académica')
 
 @section('content_header')
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <div class="d-flex justify-content-between">
         <h1>Carreras</h1>
         <a href="#" class="btn btn-primary">Agregar carrera</a>
@@ -14,14 +15,13 @@
     <div class="row">
         @foreach($careers as $career)
             <div class="col-md-4 mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $career['name'] }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $career['students'] }} estudiantes</h6>
-                        <p class="card-text">{{ $career['description'] }}</p>
-                        <a href="#" class="stretched-link"></a>
-                    </div>
-                </div>
+                <x-simple-card
+                    title="{{ $career['name'] }}"
+                    subtitle="{{ $career['students'] }} estudiantes"
+                    content="{{ $career['description'] }}"
+                    link="#"
+                    linkText="Ver detalles"
+                />
             </div>
         @endforeach
     </div>
