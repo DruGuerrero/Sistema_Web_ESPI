5bca344117ecc7ddbe0106886556dd103a8fea62
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2024-09-25 18:09:11 -0400

    View: Se mejoro el diseno de bienvenida despues del login

--- resources/views/web/admin/index.blade.php
+++ resources/views/web/admin/index.blade.php
@@ -4,18 +4,32 @@
 
 @section('content_header')
     @vite(['resources/css/app.css','resources/js/app.js'])
-    <h1>Inicio</h1>
 @stop
 
 @section('content')
-    <p>Bienvenido al panel administrativo del Instituto ESPI Bolivia.</p>
-
-    <!-- Uso del componente Livewire DonutChart -->
-    {{-- <livewire:donut-chart :series="[47, 23, 30]" :labels="['Tailwind CSS', 'Preline UI', 'Others']" id="chart1" /> --}}
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 70vh;">
+        <div class="text-center">
+            <h1 class="display-4">Bienvenido, {{ strtok(auth()->user()->name, ' ') }}!</h1>
+            <p class="lead">Bienvenido al panel administrativo del Instituto ESPI Bolivia.</p>
+        </div>
+    </div>
 @stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <style>
+        /* Opcional: Asegura que el contenedor principal ocupe toda la altura disponible */
+        .content-wrapper, .content {
+            height: 100%;
+        }
+
+        /* Opcional: Ajusta el pie de página para que no interfiera con el contenido centrado */
+        .footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
 @stop
 
 @section('js')
